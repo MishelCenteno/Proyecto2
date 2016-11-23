dca538b4084be44c0a529e52b0705eb5c1e904e4
USUARIO02-PC\Usuario01 <josuericardocando@gmail.com>
2016-11-23 11:32:06 -0500

    Escritura en el archivo creado

--- practica23112016.py
+++ practica23112016.py
@@ -1,5 +1,13 @@
 def creartxt():
     archivo=open('practica.txt','w')
     archivo.close()
+    
+def grabartxt():
+    archi =open("practica.txt","a")
+    archi.write("Mishel Centeno")
+    archi.write("Josue Cando")
+    archi.write("Programacion Avanzada")
+    archi.close()
 
-crear.txt
+creartxt()
+grabartxt()
