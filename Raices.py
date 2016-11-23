a724a4fca78dfb7f8a6fde946f80ab271db0692a
USUARIO02-PC\Usuario01 <josuericardocando@gmail.com>
2016-11-23 12:34:06 -0500

    Programa que calcula las raices de una funcion cuadratica

---
+++ Raices.py
@@ -0,0 +1,21 @@
+def factorar1(a,b,c):
+    x1 = (-b + ((b**2)-(4*a*c))**(1/2))/(2*a)
+    return x1
+
+def main():
+    a = int(input("a: "))
+    if a > 0:
+        b = int(input("b: "))
+        c = int(input("c: "))
+        discriminante  = ((b**2)-(4*a*c))**1/2
+        if discriminante >= 0:
+            resultado = factorar1(a,b,c)
+            print(resultado)
+        else:
+            print("No existen raices negativas \n\nIngrese de nuevo:")
+            main()
+    else:
+        print("Tiene que ser mayor a cero \n\nIngrese de nuevo")
+        main()
+main()
+
